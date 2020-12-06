2ac03068dad2c3e9c16916868da5c115843bde0a
Vadim91200 <62403530+Vadim91200@users.noreply.github.com>
2020-12-06 13:50:16 +0100

    Create Mot.h

---
+++ Mot.h
@@ -0,0 +1,29 @@
+#ifndef _DATE_
+#define _DATE_
+#define LGMOT 30
+/**
+ * @file Mot.h
+ * Projet SDA_Boggle
+ * @author GOICHOT, GULDAS
+ * @version 1 - 06/12/2020
+ * @brief Composant de Mot
+ * Structures de données et algorithmes - DUT1 Paris Descartes
+ */
+
+ /**
+  * @brief Structure de données de type Date
+ */
+typedef char Mot[LGMOT + 1];
+
+/**
+* @brief Recupère le mot saisi
+* @param[in] id : le mot a recupérer
+*/
+void saisir(Mot id);
+
+/**
+ * @brief Affiche un Mot
+ * @param[in] d : le Mot à afficher
+ */
+void afficher(char* d);
+#endif
