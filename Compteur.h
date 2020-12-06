36266d21a0a63640001b80617c9091c6a0d8662d
Vadim91200 <62403530+Vadim91200@users.noreply.github.com>
2020-12-06 13:40:57 +0100

    Create Compteur.h

---
+++ Compteur.h
@@ -0,0 +1,11 @@
+#ifndef _COMPTEUR_
+/**
+ * @file Compteur.h
+ * Projet SDA_Boggle
+ * @author GOICHOT, GULDAS
+ * @version 1 - 06/12/2020
+ * @brief Le compteur de point
+ * Structures de données et algorithmes - DUT1 Paris Descartes
+ */
+unsigned int Compteur();
+#endif
