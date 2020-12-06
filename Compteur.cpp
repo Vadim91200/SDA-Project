b8728be3056d3a91ed10520b14ecae6f0c29259d
Vadim91200 <62403530+Vadim91200@users.noreply.github.com>
2020-12-06 13:39:19 +0100

    Update Compteur.cpp

--- Compteur.cpp
+++ Compteur.cpp
@@ -1,1 +1,46 @@
-
+#include <iostream>
+#include <cstring>
+#include "Compteur.h"
+#include "Mot.h"
+/**
+ * @file Compteur.cpp
+ * Projet SDA_Boggle
+ * @author GOICHOT, GULDAS
+ * @version 1 - 06/12/2020
+ * @brief Le Compteur de point
+ * Structures de données et algorithmes - DUT1 Paris Descartes
+ */
+unsigned int Compteur() {
+	Mot buffer;
+	unsigned int NbPoint = 0;
+	unsigned int Taille;
+	do {
+		saisir(buffer);
+		Taille = strlen(buffer);
+		if (Taille <= 2) {
+			NbPoint += 0;
+		}
+		else if (Taille == 3 || Taille == 4)
+		{
+			NbPoint += 1;
+		}
+		else if (Taille == 5)
+		{
+			NbPoint += 2;
+		}
+		else if (Taille == 6)
+		{
+			NbPoint += 3;
+		}
+		else if (Taille == 7)
+		{
+			NbPoint += 5;
+		}
+		else if (Taille >= 8)
+		{
+			NbPoint += 11;
+		}
+	} while (strcmp(buffer, "*") == 1);
+	std::cout << NbPoint;
+	return 0;
+}
