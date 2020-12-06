6ae2a261f5ce393e75f4b215f8375deaa73026ab
Vadim91200 <62403530+Vadim91200@users.noreply.github.com>
2020-12-06 13:47:22 +0100

    Create Mot.cpp

---
+++ Mot.cpp
@@ -0,0 +1,30 @@
+/**
+ * @file Mot.cpp
+ * Projet SDA_Boggle
+ * @author GOICHOT, GULDAS
+ * @version 1 - 06/12/2020
+ * @brief Composant de Mot
+ * Structures de données et algorithmes - DUT1 Paris Descartes
+ */
+
+#include "Mot.h"
+#include <iostream>
+  /**
+  * @brief Saisie d'une date
+  * @return la date saisie
+  */
+void saisir(Mot id) {
+	std::cin >> id;
+}
+
+/**
+ * @brief Affiche une date
+ * @param[in] d : la date à afficher
+ */
+void afficher(char* d) {
+	for (unsigned int i = 0; i < strlen(d); i++)
+	{
+		std::cout << d[i];
+	}
+	std::cout << std::endl;
+}
