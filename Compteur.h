9561f6f135717f09e7dcbb7f27eb450d52f4ddb7
Vadim91200 <62403530+Vadim91200@users.noreply.github.com>
2020-12-19 10:52:39 +0100

    Update Compteur.h

--- Compteur.h
+++ Compteur.h
@@ -1,20 +1,26 @@
 #ifndef _COMPTEUR_
 #include "Liste.h"
 /**
- * @file Projet.cpp
+ * @file Compteur.h
  * Projet SDA_Boggle
  * @author GOICHOT, GULDAS
  * @version 1 - 06/12/2020
- * @brief Fichier comportant toute les fonctions develloper pour le projet
+ * @brief Le compteur de point
  * Structures de données et algorithmes - DUT1 Paris Descartes
  */
  /**
-  * @brief Affichage d'une date
+  * @brief Compte le nombre de points d'une liste
   */
-void Compteur();
+void compteur();
 /**
 * @brief Affiche une liste sous forme canonique
-* @param[In] La liste a afficher
+* @param[In] La liste a afficher 
 */
-void Canonique(Liste dMot);
+void canonique(Liste dMot);
+/**
+* @brief Affiche les mots de la seconde liste n'apparaissant pas dans la premiere
+* @param[In] La première liste
+* @param[In] La seconde liste 
+*/
+void extraction(ConteneurTDE LMot1, ConteneurTDE LMot2);
 #endif
