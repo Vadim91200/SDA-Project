18d5e3d42b574f7663ffed02cdd85c555a35adf3
Vadim91200 <62403530+Vadim91200@users.noreply.github.com>
2020-12-19 10:57:48 +0100

    Create Item.h

---
+++ Item.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "Mot.h"
+/**
+ * @file Item.h
+ * Projet sem04-tp-Cpp1-2
+ * @author l'équipe pédagogique 
+ * @version 1 - 29/11/2014
+ * @brief Spécialisation du type Item
+ * Structures de données et algorithmes - DUT1 Paris Descartes
+ */
+typedef Mot Item;
